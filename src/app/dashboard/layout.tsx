a203b716b61e5208e6f00647fd4d8b5daa927ded
Anna Hilla <annahilla21@gmail.com>
2025-02-07 15:40:12 +0100

    Add protected routes to only allow access to dashboard to logged users

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,11 +2,12 @@
 
 import { fetchIngredients } from "@/lib/store/apis/ingredientsSlice";
 import { fetchUnits } from "@/lib/store/apis/unitsSlice";
-import { useAppDispatch } from "@/lib/store/reduxHooks";
+import { useAppDispatch, useAppSelector } from "@/lib/store/reduxHooks";
 import { Suspense, useState } from "react";
 import { useEffect } from "react";
 import Loading from "@/components/ui/Loading";
 import SideNavbar from "@/components/SideNavbar";
+import ProtectedRoute from "@/utils/ProtectedRoute";
 
 export default function DashboardLayout({
   children,
@@ -15,6 +16,7 @@ export default function DashboardLayout({
 }>) {
   const dispatch = useAppDispatch();
   const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(false);
+  const isLoggedIn = useAppSelector((state) => state.auth.user) ? true : false;
 
   useEffect(() => {
     dispatch(fetchUnits());
@@ -22,23 +24,25 @@ export default function DashboardLayout({
   }, [dispatch]);
 
   return (
-    <main className="flex flex-col md:flex-row h-screen">
-      <div
-        className={`transition-all duration-300 ${
-          isNavbarCollapsed ? "md:w-16" : "md:w-64"
-        }`}
-      >
-        <SideNavbar
-          isNavbarCollapsed={isNavbarCollapsed}
-          setIsNavbarCollapsed={setIsNavbarCollapsed}
-        />
-      </div>
-
-      <Suspense fallback={<Loading />}>
-        <div className="flex-1 mx-10 my-6 h-full">
-          <div className="mb-16 md:mb-0 h-full md:w-full">{children}</div>
+    <ProtectedRoute canActivate={isLoggedIn} redirectPath="/login">
+      <main className="flex flex-col md:flex-row h-screen">
+        <div
+          className={`transition-all duration-300 ${
+            isNavbarCollapsed ? "md:w-16" : "md:w-64"
+          }`}
+        >
+          <SideNavbar
+            isNavbarCollapsed={isNavbarCollapsed}
+            setIsNavbarCollapsed={setIsNavbarCollapsed}
+          />
         </div>
-      </Suspense>
-    </main>
+
+        <Suspense fallback={<Loading />}>
+          <div className="flex-1 mx-10 my-6 h-full">
+            <div className="mb-16 md:mb-0 h-full md:w-full">{children}</div>
+          </div>
+        </Suspense>
+      </main>
+    </ProtectedRoute>
   );
 }
---
+++ src/utils/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { ReactNode } from "react";
+
+interface ProtectedRouteProps {
+  canActivate: boolean;
+  redirectPath: string;
+  children: ReactNode;
+}
+
+const ProtectedRoute = ({
+  canActivate,
+  redirectPath,
+  children,
+}: ProtectedRouteProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!canActivate) {
+      router.push(redirectPath);
+    }
+  }, [canActivate, redirectPath, router]);
+
+  if (!canActivate) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
