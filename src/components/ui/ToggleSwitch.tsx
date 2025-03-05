7945b43a4185532d8b200181fce4dbe4fbd95de1
Anna Hilla <annahilla21@gmail.com>
2025-03-05 23:33:13 +0100

    Fix toggle style on mobile

--- src/components/ui/ToggleSwitch.tsx
+++ src/components/ui/ToggleSwitch.tsx
@@ -6,19 +6,21 @@ export const ToggleSwitch = ({
   setIsOn: (value: boolean) => void;
 }) => {
   return (
-    <button
-      onClick={() => setIsOn(!isOn)}
-      className={`w-12 h-6 flex items-center rounded-full p-1 transition-colors duration-300 ${
-        isOn ? "bg-lightBrown" : "bg-gray-300"
-      } focus:outline-none`}
-      aria-pressed={isOn}
-      type="button"
-    >
-      <span
-        className={`w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-          isOn ? "translate-x-5" : "translate-x-0"
-        }`}
-      ></span>
-    </button>
+    <div>
+      <button
+        onClick={() => setIsOn(!isOn)}
+        className={`w-12 h-6 flex items-center rounded-full p-1 transition-colors duration-300 ${
+          isOn ? "bg-lightBrown" : "bg-gray-300"
+        } focus:outline-none`}
+        aria-pressed={isOn}
+        type="button"
+      >
+        <span
+          className={`w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+            isOn ? "translate-x-5" : "translate-x-0"
+          }`}
+        ></span>
+      </button>
+    </div>
   );
 };
